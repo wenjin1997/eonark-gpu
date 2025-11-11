8dd27e8af0d805f772b15fa21f44482b944f2981
Jade <71911495+wenjin1997@users.noreply.github.com>
2025-11-11 18:39:22 +0800

    modify script

--- scripts/profile_mimchasher.sh
+++ scripts/profile_mimchasher.sh
@@ -26,8 +26,9 @@ nsys profile \
   --force-overwrite=true \
   --sample=process-tree \
   --cpuctxsw=system-wide \
-  --trace=cuda,osrt,nvtx \
+  --trace=cuda,osrt,nvtx,gpu \
   --cuda-memory-usage=true \
+  --gpu-metrics-device=all \
   go run -tags icicle ./examples/mimchasher/main.go -count=1 -v
 
 echo "[nsys] 生成概览报告 (${SUMMARY_REPORT}.txt)"
