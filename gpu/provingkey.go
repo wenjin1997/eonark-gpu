671e4161be4b4de49ac176aa44e82c2bd2f5d35a
Jade Xie <wenjin1997@126.com>
2025-11-27 03:59:00 +0000

    add logs

--- gpu/provingkey.go
+++ gpu/provingkey.go
@@ -116,16 +116,15 @@ func (di *deviceInfo) initMsmPrecomputeLag(N int) error {
 
 	// 根据 N 选择 precompute_factor 和 c
 	// 只对大 MSM 开启预计算（N >= 2^21）
-	if N >= 2097152 { // N >= 2^21
+	if N >= 512 { // N >= 2^21
 		if N >= 8388608 { // N >= 2^23，包括 8388610 等
 			cfg.PrecomputeFactor = 3
-			// cfg.C = 22
+			// cfg.C = 20
 		} else if N >= 4194304 { // 2^22 <= N < 2^23
 			cfg.PrecomputeFactor = 2 // 不要用 4，否则会 OOM + fallback
 			// cfg.C = 14
 		} else { // 2^21 <= N < 2^22
-			cfg.PrecomputeFactor = 2
-			// cfg.C = 12
+			cfg.PrecomputeFactor = 5
 		}
 	} else {
 		// 小规模 MSM，不使用预计算
@@ -179,18 +178,17 @@ func (di *deviceInfo) initMsmPrecomputeG1(N int) error {
 
 	// 根据 N 选择 precompute_factor 和 c
 	// 只对大 MSM 开启预计算（N >= 2^21）
-	if N >= 2097152 { // N >= 2^21
-		if N >= 8388608 { // N >= 2^23 （含 8388610）
+	if N >= 512 { // N >= 2^21
+		if N > 8388608 {
+			cfg.PrecomputeFactor = 1
+		} else if N == 8388608 { // N >= 2^23 （含 8388610）
 			// quotient 阶段 GPU 已经很满了，这里不要再用预计算，避免 fallback 到 sequential
-			cfg.PrecomputeFactor = 3
-			// cfg.C = 16 // 可以给个固定 c 让窗口稍大一点
-			// cfg.BatchSize = 3
+			cfg.PrecomputeFactor = 1
+			// cfg.C = 16
 		} else if N >= 4194304 { // 2^22 <= N < 2^23
 			cfg.PrecomputeFactor = 2
-			cfg.C = 14
 		} else { // 2^21 <= N < 2^22
-			cfg.PrecomputeFactor = 2
-			cfg.C = 12
+			cfg.PrecomputeFactor = 6
 		}
 	} else {
 		// 小规模 MSM，不使用预计算
